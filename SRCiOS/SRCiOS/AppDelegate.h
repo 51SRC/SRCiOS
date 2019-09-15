0a8695ce0afc122da920d21af202befa7b946d2f
TianYuan <yuan.tian@timanetworks.com>
2019-09-15 13:24:50 +0800

    初始化

---
+++ SRCiOS/SRCiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRCiOS
+//
+//  Created by TianYuan on 2019/9/15.
+//  Copyright © 2019 TianYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SRCiOS/SRCiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SRCiOS
+//
+//  Created by TianYuan on 2019/9/15.
+//  Copyright © 2019 TianYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
